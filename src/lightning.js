31e6431629ea7cc68e9b4f081a317751429e923e
Sushant-Coder-01 <sushantmangore45@gmail.com>
2025-07-05 23:51:21 +0530

    Refactor lighting setup by removing commented code and ensuring shadow properties are set correctly

--- src/lightning.js
+++ src/lightning.js
@@ -1,21 +1,19 @@
-import { AmbientLight, DirectionalLight } from "three";
-import { RGBELoader } from "three/examples/jsm/Addons.js";
+import { AmbientLight } from "three";
 import { scene } from "./scene";
 import * as THREE from "three";
 
 export const lightning = () => {
-  // Ambient light for base fill
   const ambientLight = new AmbientLight(0xffffff, 1);
   scene.add(ambientLight);
 
   const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
   directionalLight.position.set(0, 10, 0);
-  // directionalLight.castShadow = true;
+  directionalLight.castShadow = true;
 
-  // directionalLight.shadow.mapSize.width = 1024;
-  // directionalLight.shadow.mapSize.height = 1024;
-  // directionalLight.shadow.camera.near = 0.5;
-  // directionalLight.shadow.camera.far = 50;
+  directionalLight.shadow.mapSize.width = 1024;
+  directionalLight.shadow.mapSize.height = 1024;
+  directionalLight.shadow.camera.near = 0.5;
+  directionalLight.shadow.camera.far = 50;
 
   scene.add(directionalLight);
 
@@ -24,14 +22,5 @@ export const lightning = () => {
   spotLight.castShadow = true;
   scene.add(spotLight);
 
-  scene.background = new THREE.Color(0x101010);
-
-  // Load HDRI for realistic environment reflections
-  // const rgbeLoader = new RGBELoader();
-  // rgbeLoader.load("scene_1.hdr", function (texture) {
-  //   texture.mapping = THREE.EquirectangularReflectionMapping;
-
-  // scene.background = texture;
-  // scene.environment = texture;
-  // });
+  scene.background = new THREE.Color(0x1a1a1a);
 };
