8f3d4b17405d856164494be0e6728e496ebe1a9a
Sushant-Coder-01 <sushantmangore45@gmail.com>
2025-07-06 16:16:20 +0530

    Add check toast functionality: display a message when the king is in check

--- src/selectModels/moveModel.js
+++ src/selectModels/moveModel.js
@@ -8,10 +8,26 @@ export function moveModelToValidTile(model, tile) {
   movePieceToTile(model, tile, () => {
     const opponentColor =
       model.userData.color === PIECES.white ? PIECES.black : PIECES.white;
+
     const info = isKingInCheck(opponentColor, getBoardState());
+
     if (info?.isInCheck) {
       console.log(`${info.king.model.name} is in CHECK!`);
+      showCheckToast();
+    } else {
+      disableCheckToast();
     }
+
     clearPreviousHighlights();
   });
+}
+
+function showCheckToast() {
+  const toast = document.getElementById("check-toast");
+  toast.style.display = "block";
+}
+
+function disableCheckToast() {
+  const toast = document.getElementById("check-toast");
+  toast.style.display = "none";
 }
